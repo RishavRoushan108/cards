74ac9cc6276d2d83f35cc502dbe03abf99ad0ed9
rishavroushan98 <rishavroushan98@gmail.com>
2025-11-03 22:57:42 +0530

    adding firebase

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkzBcvog1cAU20K1NUv663JfSy9P3Yx54",
+  authDomain: "cart-35f98.firebaseapp.com",
+  projectId: "cart-35f98",
+  storageBucket: "cart-35f98.appspot.com",
+  messagingSenderId: "971804855895",
+  appId: "1:971804855895:web:8bdcb06493a81fe3415d12"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
